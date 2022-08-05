abac6b0e28137e487f56b1cf203b0578f25d4908
DanielBarragan96 <ie702554@iteso.mx>
2022-08-05 18:13:29 -0500

    listing all movies and displaying selected name

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+
+function MoviesHeader({ movies }) {
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+  let header = !movies?.length ? (
+    <p>This list is empty</p>
+  ) : (
+    <>
+      <ul>
+        {movies.map((movie) => (
+          <button
+            key={movie}
+            onClick={() => {
+              setSelectedMovie(movie);
+            }}
+          >
+            {movie}
+          </button>
+        ))}
+      </ul>
+    </>
+  );
+  let singleMovie = !selectedMovie ? (
+    <p>This list is empty</p>
+  ) : (
+    <p>{selectedMovie}</p>
+  );
+  return (
+    <>
+      {header} {singleMovie}
+    </>
+  );
+}
+
+function MovieCard({ movie }) {
+  return !movie?.length ? (
+    <p>This list is empty</p>
+  ) : (
+    <>
+      <ul></ul>
+    </>
+  );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [movies, setMovies] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch(`https://owen-wilson-wow-api.herokuapp.com/wows/movies`)
+      .then((response) => response.json())
+      .then(setMovies)
+      .then(() => setLoading(false))
+      .catch(setError);
+  }, []);
+
+  if (loading) return <h1>Loading...</h1>;
+  if (error) return <pre>{JSON.stringify(error)}</pre>;
+  if (!movies) return null;
+  return <MoviesHeader movies={movies} />;
 }
 
 export default App;
